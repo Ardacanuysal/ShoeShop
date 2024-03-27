500559a31260b507d2137233558f46d51fb368c2
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2024-03-27 16:05:59 +0300

    Pages-images
    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './MainPage';
+import App from './src/screens/MainPage';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
